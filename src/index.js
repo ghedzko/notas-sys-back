10b91b869e7bdd93ed8cb67f85d0d30ba3b9a094
ghedzko <ghedzko@gmail.com>
2021-05-05 20:42:03 -0300

    Estructura de rutas y .env para configuración de variables de entorno

---
+++ src/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const app = require("../src/app");
+async function main(){
+    await app.listen(app.get("port"));
+    console.log("Servidor en puerto ",app.get("port"))
+}
+main();
