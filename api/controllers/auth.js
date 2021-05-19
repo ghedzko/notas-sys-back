98cfd413830590ff889cac55872dd3aef8ea8073
salva <spalmiciano@digitalhouse.com>
2021-05-18 22:07:14 -0300

    feat: add auth endpoint.

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,13 +1,13 @@
-// @deprecated
 const jwt = require("jsonwebtoken")
 
 const signIn = (req, res) => {
-    const token = jwt.sign({ id: 3 }, process.env.SECRET, {
+    const token = jwt.sign({ id: 3 }, "SECRET", {
         expiresIn: 86400, // 24 hours
     })
 
     res.json({ token })
 }
+
 module.exports = {
     signIn,
 }
