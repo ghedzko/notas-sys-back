1a8ef38f74aca3b323cf2eee2fcef02c0e7175e9
ghedzko <ghedzko@gmail.com>
2021-05-27 20:03:44 -0300

    log de heroku

--- src/database.js
+++ src/database.js
@@ -1,8 +1,5 @@
 const db = require("mongoose");
-console.log(
-  "######################################################",
-  process.env.MONGODB_URI
-);
+
 db.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
