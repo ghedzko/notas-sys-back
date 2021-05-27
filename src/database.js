9aabea97c64e1e3e4a69656072bb20faaaa284a6
ghedzko <ghedzko@gmail.com>
2021-05-27 19:54:10 -0300

    log de heroku

--- src/database.js
+++ src/database.js
@@ -1,12 +1,13 @@
-const db = require('mongoose');
-
-db
-    .connect(process.env.MONGODB_URI,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false 
-    }
-        )
-    .then((db)=>console.log('DB is connected'))
-    .catch((err) => console.log(err));
+const db = require("mongoose");
+console.log(
+  "######################################################",
+  process.env.MONGODB_URI
+);
+db.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+})
+  .then((db) => console.log("DB is connected"))
+  .catch((err) => console.log(err));
